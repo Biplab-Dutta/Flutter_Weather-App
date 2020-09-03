db493c4b2e84e808fd7fb31a8ddaaeee7b9982b6
Biplab-Dutta <biplabdutta860@gmail.com>
2020-09-03 07:27:53 +0545

    updated packages and made some minor changes.

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -4,8 +4,8 @@ class Location {
   double latitude, longitude;
   Future<void> getCurrentLocation() async {
     try {
-      Position position = await Geolocator()
-          .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+      Position position =
+          await getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
       latitude = position.latitude;
       longitude = position.longitude;
     } catch (e) {
